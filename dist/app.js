5a179731f83af7aa2834d0bccd3642c2c65ae8e8
SiddikArim <arim99999@gmail.com>
2023-12-04 17:31:11 +0600

    model and interface created

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,16 @@
 "use strict";
-const express = require("express");
-const app = express();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const cors_1 = __importDefault(require("cors"));
 const port = 3000;
+// parsers
+app.use(express_1.default.json()); // for parsing application/json
+app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express, { Application, Request, Response } from "express";
 const app: Application = express();
+import cors from "cors";
 const port = 3000;
+
+// parsers
+app.use(express.json()); // for parsing application/json
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
